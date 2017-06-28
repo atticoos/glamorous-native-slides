07122c7f29ca577a5eebaba4fcf83e6a7ecc4fb6
Atticus White <contact@atticuswhite.com>
2017-06-28 11:53:35 -0400

    updates

--- presentation/old.js
+++ presentation/old.js
@@ -44,9 +44,7 @@ const images = {
   hoc: require('../assets/HOC2.png'),
   hocPipeline: require('../assets/HOC-pipeline.png'),
   rooms: require('../assets/rooms.png'),
-  styleTimeline: require('../assets/style-timeline.png'),
-  robinGlamorous: require('../assets/robin-glamorous-example.png'),
-  roomsSystemRows: require('../assets/rooms-system-rows.png')
+  styleTimeline: require('../assets/style-timeline.png')
 };
 
 preloader(images);
@@ -160,7 +158,6 @@ export default class Presentation extends React.Component {
             What this talk is <strong>not</strong>
           </SlideTitle>
           <List>
-            <ListItem>Long!</ListItem>
             <ListItem>Free of tradeoffs</ListItem>
             <ListItem>The best way to style components</ListItem>
           </List>
@@ -356,41 +353,248 @@ export default class Presentation extends React.Component {
 
 
 
+
+
         <Slide transition={[]} bgColor="primary">
-          <SlideTitle>Themes</SlideTitle>
+          <SlideTitle>Higher Order Components</SlideTitle>
           <CodePane
             lang="js"
-            source={require("raw-loader!../assets/examples/theme-rule.example")}
+            source={require("raw-loader!../assets/hoc-text-composed.example")}
           />
+        </Slide>
+
+        <Slide transition={[]} bgColor="primary">
+          <SlideTitle>Higher Order Components</SlideTitle>
+          <CodePane
+            lang="js"
+            source={require("raw-loader!../assets/theme.example")}
+          />
+        </Slide>
+
+        <Slide transition={[]} bgColor="primary">
+          <SlideTitle>Higher Order Components</SlideTitle>
+          <Image src={images.hoc} margin="0 auto" />
+        </Slide>
+
+        <Slide transition={[]} bgColor="primary">
+          <SlideTitle>Higher Order Components</SlideTitle>
+          <CodePane
+            lang="js"
+            source={require("raw-loader!../assets/hoc-text-normal.example")}
+          />
+        </Slide>
+
+        <CodeSlide
+          transition={["zoom"]}
+          bgColor="#2d2d2d"
+          lang="js"
+          code={require("raw-loader!../assets/hoc-text-normal.example")}
+          ranges={[
+            {loc: [0, 10000], title: 'HOC'},
+            {loc: [0, 11], note: 'Wrapping component'},
+            {loc: [2, 9], note: 'Wrapped component'},
+            {loc: [12, 19], note: 'Component styles'},
+            {loc: [4, 5], note: 'Styles'},
+            {loc: [7, 8], note: 'Props'}
+          ]}
+        />
+
+
+        <Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
+          <Heading size={4} textColor="secondary" style={{marginBottom: 50}}>We're not much different</Heading>
+          <Layout>
+            <Fill>
+              <Text textSize="1em" textColor="secondary">Web</Text>
+              <CodePane
+                style={{marginRight: '5px', marginTop: 10, height: 430}}
+                lang="js"
+                source={require("raw-loader!../assets/hoc-text-web-normal.example")}
+              />
+            </Fill>
+
+            <Fill>
+              <Text textSize="1em" textColor="secondary">Native</Text>
+              <CodePane
+                style={{marginLeft: '5px', marginTop: 10, height: 430}}
+                lang="js"
+                source={require("raw-loader!../assets/hoc-text-normal.example")}
+              />
+            </Fill>
+          </Layout>
+        </Slide>
+
+
+        {/*<Slide transition={[]} bgColor="primary">
+          <SlideTitle>Higher Order Functions</SlideTitle>
+          <CodePane
+            lang="js"
+            source={require('raw-loader!../assets/hoc.example')}
+          />
+        </Slide>
+
+        <Slide transition={[]} bgColor="primary">
+          <SlideTitle>Composable Higher Order Functions</SlideTitle>
+          <CodePane
+            lang="js"
+            source={require('raw-loader!../assets/hoc-composed.example')}
+          />
+        </Slide>
+
+        <CodeSlide
+          transition={["zoom"]}
+          bgColor="#2d2d2d"
+          lang="js"
+          code={require("raw-loader!../assets/hoc-composed.example")}
+          ranges={[
+            {loc: [0, 10000], title: 'Composable HOF'},
+
+            {loc: [15, 16], note: 'Composing a filter'},
+            {loc: [9, 14], note: 'Composing a filter'},
+
+            {loc: [9, 10], note: 'The input'},
+            {loc: [10, 14], note: '`age` is within the closure'},
+            {loc: [10, 11], note: 'Called with each user in the array'},
+            {loc: [11, 12], note: 'Compare the input with the age in the `closure`'}
+          ]}
+        />*/}
+
+
+        {/*<Slide bgColor="#FFF2F2" notes={`
+  <ul>
+    <li>Kent C Dodds</li>
+    <li>Inspiration (styled-components)</li>
+    <li>API to create components with certain styles</li>
+  </ul>`}>
+          <GlamorousLogo />
+          <GlamorousLive />
+        </Slide>*/}
+
+
+
+        <Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
+          <Heading size={4} textColor="secondary" style={{marginBottom: 50}}>The 🎶 and 💃</Heading>
+          <Layout>
+            <Fill>
+              <Text textSize="1em" textColor="secondary">Vanilla</Text>
+              <CodePane
+                style={{marginRight: '5px', marginTop: 10, height: 430}}
+                lang="js"
+                source={require("raw-loader!../assets/hoc-text-normal.example")}
+              />
+            </Fill>
+
+            <Fill>
+              <Text textSize="1em" textColor="secondary">glamorous</Text>
+              <CodePane
+                style={{marginLeft: '5px', marginTop: 10, height: 430}}
+                lang="js"
+                source={require("raw-loader!../assets/hoc-text-composed.example")}
+              />
+            </Fill>
+          </Layout>
+        </Slide>
+
+        <Slide transition={[]} bgColor="primary">
+          <SlideTitle>Classic Example</SlideTitle>
+          <Layout>
+            <Fill></Fill>
+            <Fill>
+              <CodePane
+                lang="jsx"
+                source={
+`<Button color="red" large>
+  Click me!
+</Button>`
+                }
+              />
+            </Fill>
+            <Fill></Fill>
+          </Layout>
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
+          <SlideTitle>Vanilla</SlideTitle>
+
+          <CodePane
+            style={{height: 400, width: 400}}
+            lang="js"
+            source={require("raw-loader!../assets/button-normal.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
           <CodePane
             lang="jsx"
-            source={require("raw-loader!../assets/examples/theme-jsx.example")}
+            source={`<Button color="red" large>
+  Hello!
+</Button>`}
           />
-          <CodePane
-            lang="js"
-            source={require("raw-loader!../assets/examples/theme-glamorous.example")}
-          />
+          <Layout>
+            <Fill>
+              {/*<Text textSize="1em" textColor="secondary">Vanilla</Text>*/}
+              <CodePane
+                style={{marginRight: 5, marginTop: 10, height: 720}}
+                lang="js"
+                source={require("raw-loader!../assets/button-normal.example")}
+              />
+            </Fill>
+
+            <Fill>
+              {/*<Text textSize="1em" textColor="secondary">glamorous</Text>*/}
+              <CodePane
+                style={{marginLeft: 5, marginTop: 10, height: 720}}
+                lang="js"
+                source={require("raw-loader!../assets/button-composed.example")}
+              />
+            </Fill>
+          </Layout>
         </Slide>
 
-        <Slide transition={["zoom"]} bgColor="primary">
-          <SlideTitle>
-            Real World Example
-          </SlideTitle>
-          <Image
-            src={images.roomsSystemRows}
-            style={{height: 600}}
-          />
-        </Slide>
+        <CodeSlide
+          transition={["zoom"]}
+          bgColor="#2d2d2d"
+          lang="js"
+          code={require("raw-loader!../assets/button-normal.example")}
+          ranges={[
+            {loc: [0, 10000], title: 'Vanilla Component'},
+            {loc: [1, 11], note: 'Processing dynamic props and styles'},
+            {loc: [14, 15], note: 'Custom underlay color'},
+            {loc: [15, 20], note: 'Custom styles'},
 
-        <Slide transition={["zoom"]} bgColor="primary">
-          <SlideTitle>
-            Real World Example
-          </SlideTitle>
-          <Image
-            src={images.robinGlamorous}
-            style={{height: 600}}
-          />
-        </Slide>
+            {loc: [16, 17], note: 'Default style'},
+            {loc: [36, 41], note: 'Default style'},
+
+            {loc: [17, 18], note: 'User provided background color'},
+
+            {loc: [18, 19], note: 'Forward user provided styles'},
+
+            {loc: [20, 21], note: 'Forward HOC properties'},
+
+            {loc: [23, 28], note: 'Similar song and dance'}
+          ]}
+        />
+
+        <CodeSlide
+          transition={["zoom"]}
+          bgColor="#2d2d2d"
+          lang="js"
+          code={require("raw-loader!../assets/button-composed.example")}
+          ranges={[
+            {loc: [0, 10000], title: 'glamorous Component'},
+            {loc: [2, 7], note: 'Not much to see here! Just prop forwarding.'},
+
+            {loc: [19, 29], note: 'glamorous TouchableHighlight'},
+            {loc: [20, 24], note: 'Static styles'},
+            {loc: [24, 28], note: 'Dynamic styles'},
+
+            {loc: [10, 17], note: 'Composable dynamic touchable props'},
+            {loc: [18, 20], note: 'Composed with dynamic props and glamorous styles'},
+
+            {loc: [31, 37], note: 'Text with dynamic styles'},
+            {loc: [3, 4], note: 'And overriding styles are passed normally'}
+          ]}
+        />
+
 
         <Slide transition={["zoom"]} bgColor="primary">
           <SlideTitle>
@@ -402,6 +606,29 @@ export default class Presentation extends React.Component {
             <ListItem>Light HOCs / "default components"</ListItem>
           </List>
         </Slide>
+
+        <Slide transition={[]} bgColor="primary">
+          <SlideTitle>Higher Order Component</SlideTitle>
+          <Image src={images.hocPipeline} margin="0 auto" />
+        </Slide>
+
+        {/*<Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
+          <Heading size={4} textColor="secondary">The 🎶 and 💃</Heading>
+          <CodePane
+            style={{maxHeight: 'auto'}}
+            lang="js"
+            source={require("raw-loader!../assets/welcome.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
+          <Heading size={4} textColor="secondary">Enter: glamorous</Heading>
+          <CodePane
+            style={{maxHeight: 'auto'}}
+            lang="js"
+            source={require("raw-loader!../assets/welcome-improved.example")}
+          />
+        </Slide>*/}
 
         <Slide transition={["zoom"]} bgColor="primary" style={{maxHeight: 'auto'}}>
           <SlideTitle>Getting Started</SlideTitle>
